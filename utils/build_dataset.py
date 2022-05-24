211db7a76d3a3964bef7535393c2ad4b450a421a
Brady Lowe <fundamentalcoding@gmail.com>
2022-05-24 14:49:30 -0600

    REFACTOR: Created Rack class to encapsulate functionality

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -1,57 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import os
+import laspy
 
 
-def read_shapes_from_json(filename):
-    """Opens a JSON file created by LabelPC and returns the list of shapes"""
-    with open(filename, 'r') as f:
-        return json.load(f)['shapes']
+class Rack:
 
+    jitter_inward_limit = 0.5
+    jitter_outward_limit = 1.0
+    buffer_amt = 1.0
 
-def get_racks_from_shapes(shapes):
-    """Returns the rack annotations from a list of LabelPC shape annotations"""
-    return [shape for shape in shapes if 'rack' in shape['label']]
+    def __init__(self, data: dict):
+        """Takes in a dictionary from a LabelPC shape annotation"""
+        self.type = data['label']
+        self.vertices = np.min(data['vertices'], axis=0), np.max(data['vertices'], axis=0)
 
+        self.fine = self.vertices
+        self.jittered = self.jitter(self.fine)
+        self.buffered = self.buffer(self.jittered)
 
-def get_min_max_vertices_from_racks(racks):
-    """
-    Returns a numpy array of rack vertices from a list of rack annotations.
-    The arrays contain 2 vertices per rack (min_vertex, max_vertex).
-    """
-    return np.asarray([(np.min(rack['vertices'], axis=0), np.max(rack['vertices'], axis=0)) for rack in racks])
+    @staticmethod
+    def jitter(rack):
+        """
+        Returns a rack annotation that has been modified randomly.
+        The points only move inward (into the rack) by at most `jitter_inward_limit`.
+        The points only move outward (out of the rack) by at most `jitter_outward_limit`.
+        """
+        sum_limit = Rack.jitter_outward_limit + Rack.jitter_inward_limit
+        offset = (Rack.jitter_outward_limit - Rack.jitter_inward_limit) / 2.0
+        jitter_1, jitter_2 = (np.random.random(2) - 0.5) * sum_limit
+        return np.array((rack[0] - offset + jitter_1, rack[1] + offset + jitter_2))
 
+    @staticmethod
+    def buffer(rack):
+        """
+        Returns a rack which has been extended outward in the x- and y-directions
+        by the amount of the given buffer.
+        To be specific, if buffer=1, then the left side of the rack is moved
+        leftward by 1 meter, the right side of the rack is moved rightward
+        by 1 meter, and similarly with the top and bottom sides of the rack.
+        """
+        return np.array((rack[0] - Rack.buffer_amt, rack[1] + Rack.buffer_amt))
 
-def add_random_jitter_to_rack(rack, inward_limit: float, outward_limit: float):
-    """
-    Returns a rack annotation that has been modified randomly.
-    The points only move inward (into the rack) by at most `inward_limit`.
-    The points only move outward (out of the rack) by at most `outward_limit`.
-    """
-    # Calculate random jitter within bounds
-    # Apply random jitter and return result
-    return np.array((rack[0] + inward_limit, rack[1] - inward_limit))
+    @staticmethod
+    def points_in_rack(points, rack):
+        """Returns a boolean mask indicating which points are contained in this rack"""
+        keep_1 = points[:, :2] >= rack[0]
+        keep_2 = points[:, :2] <= rack[1]
+        return keep_1 & keep_2
 
+    def plot(self):
+        """Plot the three rectangles on the same plot with different colors"""
+        for rack, color in zip([self.fine, self.jittered, self.buffered], ['red', 'green', 'blue']):
+            x_min, y_min = rack[0]
+            x_max, y_max = rack[1]
 
-def add_buffer_to_rack(rack, buffer: float):
-    """
-    Returns a rack which has been extended outward in the x- and y-directions
-    by the amount of the given buffer.
-    To be specific, if buffer=1, then the left side of the rack is moved
-    leftward by 1 meter, the right side of the rack is moved rightward
-    by 1 meter, and similarly with the top and bottom sides of the rack.
-    """
-    return np.array((rack[0] - buffer, rack[1] + buffer))
+            x_points = [x_min, x_max, x_max, x_min, x_min]
+            y_points = [y_min, y_min, y_max, y_max, y_min]
 
+            plt.plot(x_points, y_points, color)
+            plt.legend(['Fine', 'Rough', 'Buffered'])
 
-def plot_rack(rack, color='black'):
-    x_min, y_min = rack[0]
-    x_max, y_max = rack[1]
-
-    x_points = [x_min, x_max, x_max, x_min, x_min]
-    y_points = [y_min, y_min, y_max, y_max, y_min]
-
-    plt.plot(x_points, y_points, color)
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            data = {'fine': self.fine,
+                    'jittered': self.jittered,
+                    'buffered': self.buffered}
+            json.dump(data, f)
 
 
 if __name__ == "__main__":
@@ -67,31 +83,9 @@ if __name__ == "__main__":
     else:
         test_file = 'test/single_rack.json'
 
-    # Get the rack(s)
-    shapes = read_shapes_from_json(test_file)
-    racks = get_racks_from_shapes(shapes)
-    racks = get_min_max_vertices_from_racks(racks)
-
-    for rack in racks:
-
-        # Calculate the other racks
-        rough_rack = add_random_jitter_to_rack(rack, -0.5, 1.0)
-        buffered_rack = add_buffer_to_rack(rough_rack, 1.0)
-
-        # Plot racks
-        plot_rack(rack, 'red')
-        plot_rack(rough_rack, 'green')
-        plot_rack(buffered_rack, 'blue')
-
-    # Show the plot
-    plt.legend(['Orig', 'Jittered', 'Buffered'])
-
-    # Make the plot bigger so it looks a little nicer
-    buf = 3
-    x_lim, y_lim = plt.xlim(), plt.ylim()
-    plt.xlim([x_lim[0] - buf, x_lim[1] + buf])
-    plt.ylim([y_lim[0] - buf, y_lim[1] + buf])
-
-    # Show the plot
-    plt.show()
-
+    with open(test_file, 'r') as f:
+        for shape in json.load(f)['shapes']:
+            if 'rack' in shape['label']:
+                r = Rack(shape)
+                r.plot()
+        plt.show()
