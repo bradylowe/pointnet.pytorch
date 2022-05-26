d83886ef3fc50ca8aaad5d88e73568e4966a7335
Brady Lowe <fundamentalcoding@gmail.com>
2022-05-25 18:28:49 -0600

    REFACTOR: Using non-deprecated sigmoid

--- pointnet/model.py
+++ pointnet/model.py
@@ -146,7 +146,7 @@ class PointNetCls(nn.Module):
         x = F.relu(self.bn1(self.fc1(x)))
         x = F.relu(self.bn2(self.dropout(self.fc2(x))))
         x = self.fc3(x)
-        return F.sigmoid(x) - 0.5, trans, trans_feat
+        return torch.sigmoid(x) - 0.5, trans, trans_feat
 
 
 class PointNetDenseCls(nn.Module):
