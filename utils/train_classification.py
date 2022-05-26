e855ac4e1542d728cb7dc5f7871ce4f87f7fb1e9
Brady Lowe <fundamentalcoding@gmail.com>
2022-05-25 18:32:05 -0600

    REFACTOR: Swapped out loss function

--- utils/train_classification.py
+++ utils/train_classification.py
@@ -6,7 +6,6 @@ import torch.optim as optim
 import torch.utils.data
 from pointnet.dataset import LasDataset
 from pointnet.model import PointNetCls, feature_transform_regularizer
-import torch.nn.functional as F
 from torch.nn import MSELoss, L1Loss
 from tqdm import tqdm
 
@@ -92,6 +91,7 @@ if using_cuda:
     classifier.cuda()
 
 num_batch = len(dataset) / opt.batchSize
+loss_function = MSELoss(reduction='sum')
 
 for epoch in range(opt.nepoch):
     scheduler.step()
@@ -103,7 +103,7 @@ for epoch in range(opt.nepoch):
         optimizer.zero_grad()
         classifier = classifier.train()
         pred, trans, trans_feat = classifier(points)
-        loss = MSELoss(pred, target)
+        loss = loss_function(pred, target)
         if opt.feature_transform:
             loss += feature_transform_regularizer(trans_feat) * 0.001
         loss.backward()
@@ -118,7 +118,7 @@ for epoch in range(opt.nepoch):
                 points, target = points.cuda(), target.cuda()
             classifier = classifier.eval()
             pred, _, _ = classifier(points)
-            loss = F.nll_loss(pred, target)
+            loss = loss_function(pred, target)
             print('[%d: %d/%d] %s loss: %f' % (epoch, i, num_batch, blue('test'), loss.item()))
 
     torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
@@ -132,7 +132,7 @@ for i,data in tqdm(enumerate(testdataloader, 0)):
         points, target = points.cuda(), target.cuda()
     classifier = classifier.eval()
     pred, _, _ = classifier(points)
-    total_loss += MSELoss(pred, target)
+    total_loss += loss_function(pred, target)
     total_testset += points.size()[0]
 
 print("final average loss {}".format(total_loss / float(total_testset)))
