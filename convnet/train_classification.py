7fc92241fb7f85eb0d2c8a63c21adc31fe619ddd
Brady Lowe <brady.lowe@endava.com>
2022-06-15 17:35:09 -0600

    FEAT: Created a training algorithm for the convnet

---
+++ convnet/train_classification.py
@@ -0,0 +1,130 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch.optim as optim
+import torch.utils.data
+from convnet.dataset import LasDatasetSlices
+from convnet.model import SimpleConv2d
+from torch.nn import MSELoss
+from tqdm import tqdm
+from utils.plot_data import plot_array, plot_arrays
+
+os.system('color')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--batchSize', type=int, default=4, help='input batch size')
+parser.add_argument(
+    '--resolution', type=int, default=512, nargs='+', help='Image resolution')
+parser.add_argument(
+    '--workers', type=int, default=0, help='number of data loading workers')
+parser.add_argument(
+    '--nepoch', type=int, default=250, help='number of epochs to train for')
+parser.add_argument('--outf', type=str, default='cls', help='output folder')
+parser.add_argument('--model', type=str, default='', help='model path')
+parser.add_argument('--train_dataset', type=str, required=True, help="Training dataset path")
+parser.add_argument('--test_dataset', type=str, required=True, help="Testing dataset path")
+
+using_cuda = torch.cuda.is_available()
+if using_cuda:
+    print('We are using cuda')
+else:
+    print('We are NOT using cuda')
+
+opt = parser.parse_args()
+print(opt)
+
+blue = lambda x: '\033[94m' + x + '\033[0m'
+
+opt.manualSeed = random.randint(1, 10000)  # fix seed
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+dataset = LasDatasetSlices(root=opt.train_dataset)
+test_dataset = LasDatasetSlices(root=opt.test_dataset)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=opt.batchSize,
+    shuffle=True,
+    num_workers=int(opt.workers))
+
+testdataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=opt.batchSize,
+        shuffle=True,
+        num_workers=int(opt.workers))
+
+print('Number of training/testing items:', len(dataset), '/', len(test_dataset))
+output_dim = len(dataset.output_names)
+print('Output names', dataset.output_names)
+
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+classifier = SimpleConv2d(image_resolution=opt.resolution, n_slices=dataset.n_slices)
+
+if opt.model != '':
+    classifier.load_state_dict(torch.load(opt.model))
+
+optimizer = optim.Adam(classifier.parameters(), lr=0.1, betas=(0.9, 0.999))
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+if using_cuda:
+    classifier.cuda()
+
+num_batch = len(dataset) / opt.batchSize
+loss_function = MSELoss(reduction='sum')
+
+for epoch in range(opt.nepoch):
+    for i, data in enumerate(dataloader, 0):
+        slices, target = data
+        if using_cuda:
+            slices, target = slices.cuda(), target.cuda()
+        optimizer.zero_grad()
+        classifier = classifier.train()
+        pred = classifier(slices)
+        loss = loss_function(pred, target)
+        loss.backward()
+        optimizer.step()
+        print('[%d: %d/%d] train loss: %f' % (epoch, i, num_batch, loss.item()))
+
+        if i % 10 == 0:
+            j, data = next(enumerate(testdataloader, 0))
+            slices, target = data
+            if using_cuda:
+                slices, target = slices.cuda(), target.cuda()
+            classifier = classifier.eval()
+            pred = classifier(slices)
+            loss = loss_function(pred, target)
+            print('[%d: %d/%d] %s loss: %f' % (epoch, i, num_batch, blue('test'), loss.item()))
+        scheduler.step()
+
+    if epoch % 50 == 0:
+        torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+        weights = [classifier.conv1a.weight.cpu().detach(),
+                   classifier.conv1b.weight.cpu().detach()]
+        old_weights = [w for w in weights]
+        png_dir = os.path.join(opt.train_dataset, 'png')
+        png_file = os.path.join(png_dir, f'rough_weights_epoch{epoch}_conv1.png')
+        plot_arrays(weights[0][:64, 0, :, :], save_to=png_file, shape=(8, 8), axis_labels=False)
+        png_file = os.path.join(png_dir, f'fine_weights_epoch{epoch}_conv2.png')
+        plot_arrays(weights[1][:64, 0, :, :], save_to=png_file, shape=(8, 8), axis_labels=False)
+
+
+total_loss = 0
+total_testset = 0
+for i, data in tqdm(enumerate(testdataloader, 0)):
+    slices, target = data
+    if using_cuda:
+        slices, target = slices.cuda(), target.cuda()
+    classifier = classifier.eval()
+    pred = classifier(slices)
+    total_loss += loss_function(pred, target)
+    total_testset += slices.size()[0]
+
+print("final average loss {}".format(total_loss / float(total_testset)))
