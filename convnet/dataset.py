d3f6a2eaaa4ea01ca52aefd172d5f63981122178
Brady Lowe <brady.lowe@endava.com>
2022-06-22 17:27:29 -0600

    FEAT: Can now get_resolution from a dataset

--- convnet/dataset.py
+++ convnet/dataset.py
@@ -16,7 +16,11 @@ class LasDatasetSlices(data.Dataset):
         self.pkl_files = [os.path.join(self.pkl_dir, f) for f in os.listdir(self.pkl_dir)]
         self.json_files = [os.path.join(self.json_dir, f) for f in os.listdir(self.json_dir)]
 
+        self.resolution = self.get_resolution()
         self.n_slices = self.get_n_slices()
+
+    def get_resolution(self):
+        return load_from_pkl(self.pkl_files[0]).shape[1:]
 
     def get_n_slices(self):
         return load_from_pkl(self.pkl_files[0]).shape[0]
