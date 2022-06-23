7959dc42b75b844f332313941928a04956b058e7
Brady Lowe <brady.lowe@endava.com>
2022-06-22 18:22:01 -0600

    FEAT: Can now create train/test split in dataset
    Also, if you don't want to over-write existing
    data, then you now must supply your own safe
    'count_start' value.

--- convnet/build_dataset.py
+++ convnet/build_dataset.py
@@ -8,40 +8,50 @@ from utils.rack import Rack
 from utils.scan import Scan
 
 
-def build_dataset_from_scan(scan: Scan, output_dir: str, multiplier: int = 1, min_points: int = 100000,
-                            resolution: int = 512, n_slices: int = 1,
-                            min_z: float = 0.5, max_z: float = 8.0):
+def build_dataset_from_scan(scan: Scan, output_dir: str, split: float = 0.1, multiplier: int = 1,
+                            min_points: int = 100000, resolution: int = 512, n_slices: int = 1,
+                            min_z: float = 0.5, max_z: float = 8.0, count_start: int = 0):
 
     # Set up output directories
-    pkl_dir, json_dir = os.path.join(output_dir, 'pkl'), os.path.join(output_dir, 'json')
-    if not os.path.exists(pkl_dir):
-        os.makedirs(pkl_dir)
-    if not os.path.exists(json_dir):
-        os.makedirs(json_dir)
-
-    # Make sure we don't overwrite existing files
-    count = 0
-    for filename in os.listdir(json_dir):
-        n = int(filename[5:-5])
-        if n >= count:
-            count = n + 1
+    pkl_dir_train, json_dir_train = os.path.join(output_dir, 'train', 'pkl'), os.path.join(output_dir, 'train', 'json')
+    pkl_dir_test, json_dir_test = os.path.join(output_dir, 'test', 'pkl'), os.path.join(output_dir, 'test', 'json')
+    if not os.path.exists(pkl_dir_train):
+        os.makedirs(pkl_dir_train)
+    if not os.path.exists(json_dir_train):
+        os.makedirs(json_dir_train)
+    if not os.path.exists(pkl_dir_test):
+        os.makedirs(pkl_dir_test)
+    if not os.path.exists(json_dir_test):
+        os.makedirs(json_dir_test)
 
     # Loop over racks
+    count = count_start
     for rack in scan.racks:
+
+        # Randomly shuffle rack data into train and test directories
+        if split and np.random.random() > split:
+            pkl_dir = pkl_dir_train
+            json_dir = json_dir_train
+        else:
+            pkl_dir = pkl_dir_test
+            json_dir = json_dir_test
+
+        # Loop over each rack multiple times (data augmentation)
         for _ in range(multiplier):
             rack.augment()
 
-            # Subsample LAS file and save
+            # Subsample rack from LAS file
             mask = Rack.points_in_rack(scan.pc.points, rack.buffered)
             if mask.sum() >= min_points:
 
                 # Write pkl file (bitmaps)
                 data_file = os.path.join(pkl_dir, f'rack_{count}.pkl')
-                bitmap = build_bitmap(scan.pc.points.loc[mask, ['x', 'y', 'z']], resolution, n_slices, min_z, max_z)
-                write_to_pkl(bitmap, data_file)
+                slices, scale_xy = build_bitmap(scan.pc.points.loc[mask, ['x', 'y', 'z']], resolution, n_slices, min_z, max_z)
+                write_to_pkl(slices, data_file)
 
                 # Write JSON data
                 json_file = os.path.join(json_dir, f'rack_{count}.json')
+                rack.data['scale_xy'] = scale_xy
                 rack.save(json_file)
 
                 count += 1
@@ -125,12 +135,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create dataset')
     parser.add_argument('--input_json', type=str, required=True, help='Path to input LabelPC JSON file')
     parser.add_argument('--output', type=str, help='Path at which to create new dataset')
+    parser.add_argument('--split', type=float, default=0.1, help='Percentage of data to tag as testing')
     parser.add_argument('--multiplier', type=int, default=1, help='# of times to sample each rack')
     parser.add_argument('--min_points', type=float, default=100000, help='Min number of points needed to create file')
     parser.add_argument('--resolution', type=int, default=512, help='Image resolution')
     parser.add_argument('--n_slices', type=int, default=1, help='Number of vertical slices')
     parser.add_argument('--min_z', type=float, default=0.1, help='Minimum z-height to keep')
     parser.add_argument('--max_z', type=float, default=8.0, help='Maximum z-height to keep')
+    parser.add_argument('--count_start', type=int, default=0, help='Beginning unique ID number for output files')
     parser.add_argument('--test', action='store_true', help='If True, then run tests')
     parser.add_argument('--plot', action='store_true', help='If True, just plot the dataset')
     args = parser.parse_args()
@@ -142,7 +154,7 @@ if __name__ == "__main__":
     elif args.plot:
         scan.plot()
     elif args.output:
-        build_dataset_from_scan(scan, args.output, args.multiplier, args.min_points,
-                                args.resolution, args.n_slices, args.min_z, args.max_z)
+        build_dataset_from_scan(scan, args.output, args.split, args.multiplier, args.min_points,
+                                args.resolution, args.n_slices, args.min_z, args.max_z, args.count_start)
     else:
         print('No output directory provided...')
