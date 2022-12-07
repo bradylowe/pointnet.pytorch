4ef9882cfa4b9ade64cd7917e3c45da58cc16bf3
Brady Lowe <brady.lowe@endava.com>
2022-12-07 16:16:33 -0700

    FIX: Appending to log file instead of over-writing

--- utils/log.py
+++ utils/log.py
@@ -2,7 +2,7 @@ import os.path as osp
 
 
 def log_loss(category, epoch, value, log_filename):
-    with open(log_filename, 'w') as f:
+    with open(log_filename, 'a') as f:
         if not osp.exists(log_filename):
             f.write('category,epoch,value')
         f.write(f'{category},{epoch},{value}')
