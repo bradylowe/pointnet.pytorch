a3c7dbfeb4871f38f5141939a06d600cdf6c0658
Brady Lowe <brady.lowe@endava.com>
2022-12-13 09:27:57 -0700

    REFACTOR: Merged the training scripts

--- door_pointnet/train_classification.py
+++ door_pointnet/train_classification.py
@@ -4,9 +4,9 @@ import os
 import random
 import torch.optim as optim
 import torch.utils.data
-from door_pointnet.dataset import LasDataset
-from door_pointnet.model import PointNetCls, feature_transform_regularizer
-from torch.nn import MSELoss, L1Loss
+from door_pointnet.dataset import LasDataset, PointPillarsDataset
+from door_pointnet.model import PointNetCls, PointNetPillars
+from torch.nn import MSELoss
 from tqdm import tqdm
 from utils.log import LogLoss
 
@@ -14,7 +14,7 @@ os.system('color')
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
 parser.add_argument('--num_points', type=int, default=2500, help='input batch size')
 parser.add_argument('--workers', type=int, default=0, help='number of data loading workers')
 parser.add_argument('--nepoch', type=int, default=250, help='number of epochs to train for')
@@ -22,11 +22,19 @@ parser.add_argument('--outf', type=str, default='cls', help='output folder')
 parser.add_argument('--model', type=str, default='', help='model path')
 parser.add_argument('--train_dataset', type=str, required=True, help="Training dataset path")
 parser.add_argument('--test_dataset', type=str, required=True, help="Testing dataset path")
-parser.add_argument('--dataset_type', type=str, default='las', help="dataset type")
-parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
 parser.add_argument('--log', type=str, help="Path to a log of the training and testing loss")
 parser.add_argument('--augment', action='store_true', help='Perform data augmentation')
 
+# PointPillar parameters
+parser.add_argument('--use_pillars', action='store_true', help='Use the PointPillars model')
+parser.add_argument('--points_per_pillar', type=int, default=100, help='Number of points to keep per-pillar')
+parser.add_argument('--n_pillars', type=int, default=50, help='Number of pillars to consider')
+parser.add_argument('--grid_size', type=int, default=0.25, help='Grid spacing for creating pillars')
+
+
+"""
+INITIAL SETUP
+"""
 using_cuda = torch.cuda.is_available()
 if using_cuda:
     print('We are using cuda')
@@ -45,9 +53,35 @@ print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+
+"""
+LOAD THE DATA
+"""
 dataset, test_dataset = None, None
 point_attributes = ['x', 'y', 'z']
-if opt.dataset_type == 'las':
+if opt.dataset_type == opt.use_pillars:
+    dataset = PointPillarsDataset(
+        root=opt.train_dataset,
+        grid_size=opt.grid_size,
+        points_per_pillar=opt.points_per_pillar,
+        n_pillars=opt.n_pillars,
+        point_attribs=point_attributes,
+        data_augmentation=opt.augment)
+
+    test_dataset = PointPillarsDataset(
+        root=opt.test_dataset,
+        split='test',
+        grid_size=opt.grid_size,
+        points_per_pillar=opt.points_per_pillar,
+        n_pillars=opt.n_pillars,
+        data_augmentation=False,
+        point_attribs=point_attributes)
+else:
     dataset = LasDataset(
         root=opt.train_dataset,
         npoints=opt.num_points,
@@ -60,19 +94,20 @@ if opt.dataset_type == 'las':
         npoints=opt.num_points,
         data_augmentation=False,
         point_attribs=point_attributes)
-else:
-    exit('wrong dataset type')
 
 
+"""
+BUILD DATA LOADERS
+"""
 dataloader = torch.utils.data.DataLoader(
     dataset,
-    batch_size=opt.batchSize,
+    batch_size=opt.batch_size,
     shuffle=True,
     num_workers=int(opt.workers))
 
 testdataloader = torch.utils.data.DataLoader(
         test_dataset,
-        batch_size=opt.batchSize,
+        batch_size=opt.batch_size,
         shuffle=True,
         num_workers=int(opt.workers))
 
@@ -80,12 +115,15 @@ print('Number of training/testing items:', len(dataset), '/', len(test_dataset))
 output_dim = len(dataset.output_names)
 print('Output names', dataset.output_names)
 
-try:
-    os.makedirs(opt.outf)
-except OSError:
-    pass
 
-classifier = PointNetCls(k=output_dim, feature_transform=opt.feature_transform, point_dim=dataset.point_dim)
+"""
+LOAD THE MODEL, OPTIMIZER, AND LOSS FUNCTION
+"""
+if opt.use_pillars:
+    classifier = PointNetPillars(point_dim=dataset.point_dim, output_dim=dataset.point_dim,
+                                 n_pillars=opt.n_pillars, points_per_pillar=opt.points_per_pillar)
+else:
+    classifier = PointNetCls(k=output_dim, feature_transform=opt.feature_transform, point_dim=dataset.point_dim)
 
 if opt.model != '':
     classifier.load_state_dict(torch.load(opt.model))
@@ -95,9 +133,13 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 if using_cuda:
     classifier.cuda()
 
-num_batch = len(dataset) / opt.batchSize
 loss_function = MSELoss(reduction='sum')
 
+
+"""
+TRAIN
+"""
+num_batch = len(dataset) / opt.batch_size
 for epoch in range(opt.nepoch):
     for i, data in enumerate(dataloader, 0):
         points, target = data
@@ -110,8 +152,6 @@ for epoch in range(opt.nepoch):
             continue
         pred, _, trans_feat = classifier(points)
         loss = loss_function(pred, target)
-        if opt.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
         loss.backward()
         optimizer.step()
         print('[%d: %d/%d] train loss: %f' % (epoch, i, num_batch, loss.item()))
